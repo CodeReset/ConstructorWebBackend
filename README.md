1b0c8c708b566a3aaaaf62c1c2fae1f53daab88c
Salih <mr.pagashev@gmail.com>
2021-03-23 23:49:51 +0600

    Sakif-3

---
+++ README.md
@@ -0,0 +1,2 @@
+# ConstructorWebBackend
+ConstructorWebBackend
---
+++ app.js
@@ -0,0 +1,100 @@
+import express from 'express';
+import Parser from './rnparser';
+import bodyParser from 'body-parser';
+import cors from 'cors';
+
+const app = express();
+const port = 3000;
+
+app.use(express.json({ extended: true }));
+app.use(cors());
+app.use('/static', express.static('public'));
+
+app.post('/add', (req, res) => {
+  console.log(req.body);
+  //   const parser = new Parser(req.body.id, req.body.config);
+  res.send(200);
+});
+
+app.post('/getThemes', (req, res) => {
+  console.log(req.body);
+  const themes = [];
+  res.send(200).json(themes);
+});
+
+app.post('/getPages', (req, res) => {
+  const pages = [
+    {
+      name: 'Main',
+      components: [
+        {
+          name: 'head',
+          entity: 'component',
+          text: 'Headline',
+          type: 'default',
+          props: { text: 'Деликиос фуд' }
+        },
+        {
+          name: 'search',
+          entity: 'component',
+          text: 'Search',
+          type: 'default'
+        },
+        {
+          name: 'loaderLogic',
+          entity: 'fragment',
+          type: 'start'
+        },
+        {
+          name: 'loader',
+          entity: 'component',
+          text: 'Loader',
+          type: 'default'
+        },
+        {
+          name: 'view',
+          entity: 'fragment',
+          type: 'start'
+        },
+        {
+          name: 'categoryList',
+          entity: 'component',
+          text: 'Category List',
+          type: 'default'
+        },
+        {
+          name: 'productList',
+          entity: 'component',
+          text: 'Product List',
+          type: 'horizontal'
+        },
+        {
+          name: 'view',
+          entity: 'fragment',
+          type: 'end'
+        },
+        {
+          name: 'loaderLogic',
+          entity: 'fragment',
+          type: 'end'
+        }
+      ]
+    }
+  ];
+  res.send(200).json(pages);
+});
+
+app.post('/getComponent', (req, res) => {
+  console.log(req.body);
+  const component = {
+    main: {
+      head: { text: 'Headline', props: [{ name: 'text', type: 'text' }] },
+      categoryList: { text: 'Category list', types: [{ name: 'Horizontal', src: 'horizontal' }] }
+    }
+  };
+  res.send(200).json(component[req.body.page][req.body.name]);
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
